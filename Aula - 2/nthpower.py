477014f9a2f37f6fed9a05dd68a258a2f8b4fdd4
Guilherme Vieira <88863957+guilhermevieirasilvagoncalves@users.noreply.github.com>
2022-06-15 16:37:27 -0300

    funções aula 2

---
+++ Aula - 2/nthpower.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+imagem = cv2.imread("lena.jpg",0)
+
+col = imagem.shape[1]
+lin = imagem.shape[0]
+
+imagemR = np.zeros((lin,col))
+
+for y in range(1, lin):
+    for x in range(1, col):
+        r = imagem[y,x] 
+        imagemR[y,x] = r ** 1.1 
+
+cv2.imshow("imagem Resultado",imagemR/255)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
